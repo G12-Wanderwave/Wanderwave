3c0b37703caf714c9bf087d384384f11abfb52c2
Clarence LINDEN <clarence.linden@epfl.ch>
2024-03-15 23:01:25 +0100

    update gitignore, update to changeable theme

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LaunchScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LaunchScreen.kt
@@ -1,11 +1,34 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun LaunchScreen() {
-  Text(text = "LaunchScreen Placeholder")
+  Column (
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center,
+    modifier = Modifier.fillMaxSize()
+  ){
+
+    Text(text = "Launch Screen",
+      modifier = Modifier.padding(16.dp),
+      fontWeight = FontWeight(700),
+      fontSize = 40.sp,
+    )
+
+  }
 }
 
 /*
