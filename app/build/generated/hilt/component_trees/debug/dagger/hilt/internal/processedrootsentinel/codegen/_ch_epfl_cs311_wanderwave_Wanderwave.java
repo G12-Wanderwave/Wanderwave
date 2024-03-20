9eb869b36ec4e012a7a52a96cdb8cb7c38e2ae0c
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-20 18:11:45 +0100

    remove app/build/

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_ch_epfl_cs311_wanderwave_Wanderwave.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "ch.epfl.cs311.wanderwave.Wanderwave"
-)
-public final class _ch_epfl_cs311_wanderwave_Wanderwave {
-}
