a63b42b18708d927262943ba9ebcd09e93aa3a30
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-22 12:55:53 +0100

    add property to find the testreport for sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,5 +14,6 @@ sonar {
         property("sonar.projectKey", "G12-Wanderwave_Wanderwave")
         property("sonar.organization", "g12-wanderwave")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("soner.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
     }
 }
