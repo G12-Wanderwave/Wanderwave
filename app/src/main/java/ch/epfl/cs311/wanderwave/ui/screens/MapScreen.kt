bef9ce430184752fea08c0bc8900bb1a42bf67b2
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-23 20:43:49 +0100

    add map screen with simple map view

---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
@@ -0,0 +1,11 @@
+package ch.epfl.cs311.wanderwave.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.maps.android.compose.GoogleMap
+
+@Composable
+@Preview
+fun MapScreen() {
+  GoogleMap()
+}
