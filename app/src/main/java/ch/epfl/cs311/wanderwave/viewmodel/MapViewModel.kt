e79b7874306fd0685dc3ef80bf88dcf052b5f3a2
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-24 17:08:30 +0100

    add MapViewModel and add it to the map screen

---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/MapViewModel.kt
@@ -0,0 +1,34 @@
+package ch.epfl.cs311.wanderwave.viewmodel
+
+import androidx.lifecycle.ViewModel
+import ch.epfl.cs311.wanderwave.model.data.Beacon
+import ch.epfl.cs311.wanderwave.model.repository.BeaconRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MapViewModel @Inject constructor(private val repository: BeaconRepository) :
+  ViewModel() {
+
+  private val _uiState = MutableStateFlow(BeaconListUiState(loading = true))
+  val uiState: StateFlow<BeaconListUiState> = _uiState
+
+  init {
+    observeBeacons()
+  }
+
+  private fun observeBeacons() {
+    CoroutineScope(Dispatchers.IO).launch {
+      repository.getAll().collect { beacons ->
+        _uiState.value = BeaconListUiState(beacons = beacons, loading = false)
+      }
+    }
+  }
+}
+
+data class BeaconListUiState(val beacons: List<Beacon> = listOf(), val loading: Boolean = false)
