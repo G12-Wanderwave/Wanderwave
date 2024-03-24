06de3a411b9306e8fcaedc65712f14520a31cc9e
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-24 17:08:11 +0100

    add beacon data class and repo to get them

---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/data/Beacon.kt
@@ -0,0 +1,13 @@
+package ch.epfl.cs311.wanderwave.model.data
+
+data class Beacon(
+
+  /** GUID of the beacon */
+  val id: String,
+
+  /** Location of the beacon */
+  val location: Location,
+
+  /** List of tracks that are broadcast from the beacon */
+  val tracks: List<Track> = listOf(),
+)
