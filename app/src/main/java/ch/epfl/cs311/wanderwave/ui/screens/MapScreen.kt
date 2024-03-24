e67273fab5e2d72c92d9551a5b4c96ed00746a85
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-03-24 17:08:30 +0100

    add MapViewModel and add it to the map screen

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
@@ -1,11 +1,30 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ch.epfl.cs311.wanderwave.viewmodel.MapViewModel
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @Composable
 @Preview
 fun MapScreen() {
-  GoogleMap()
+  val viewModel : MapViewModel = hiltViewModel()
+  val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+  GoogleMap() {
+    uiState.beacons.forEach() {
+      // Add a marker for each beacon
+      val position = it.location
+      Marker(
+        state = MarkerState(position = position.toLatLng()),
+        title = it.id
+      )
+      // MarkerOptions().position(LatLng(it.latitude, it.longitude)).title(it.title)
+    }
+  }
 }
