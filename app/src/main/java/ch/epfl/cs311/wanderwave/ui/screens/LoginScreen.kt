4ccb5989192bd6cc4d241887a54e450ef1b79ce4
Yoric Züger <79338025+yzueger@users.noreply.github.com>
2024-03-25 23:28:51 +0100

    Create UI Component to get a Spotify login token

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LoginScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LoginScreen.kt
@@ -2,22 +2,31 @@ package ch.epfl.cs311.wanderwave.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import ch.epfl.cs311.wanderwave.ui.components.login.LoginScreenHeader
 import ch.epfl.cs311.wanderwave.ui.components.login.SignInButton
+import ch.epfl.cs311.wanderwave.ui.components.login.SpotifyAuthentication
 import ch.epfl.cs311.wanderwave.ui.components.login.WelcomeTitle
 import ch.epfl.cs311.wanderwave.ui.navigation.NavigationActions
-import ch.epfl.cs311.wanderwave.ui.navigation.Route
-import ch.epfl.cs311.wanderwave.ui.navigation.TOP_LEVEL_DESTINATIONS
 
 @Composable
 fun LoginScreen(navigationActions: NavigationActions) {
+  var spotifyLoginShown: Boolean by remember { mutableStateOf(false) }
+
   Column(modifier = Modifier.testTag("loginScreen")) {
     LoginScreenHeader(modifier = Modifier.weight(1.5f))
     WelcomeTitle(modifier = Modifier.weight(4f))
-    SignInButton(modifier = Modifier.weight(1f)) {
-      navigationActions.navigateTo(TOP_LEVEL_DESTINATIONS.first { it.route == Route.MAIN })
+    SignInButton(modifier = Modifier.weight(1f)) { spotifyLoginShown = true }
+    if (spotifyLoginShown) {
+      SpotifyAuthentication(
+          onSuccess = { token, expiresIn -> println("Logged in with token: $token, expiresIn: $expiresIn") },
+          onError = { println("Error logging in: $it") },
+          onCancel = { println("User cancelled login") })
     }
   }
 }
