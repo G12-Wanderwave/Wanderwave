4f7579d50cfa612d4e2799d18e7dd948016a6535
Imade <imade.bouhamria@epfl.ch>
2024-03-26 22:13:47 +0100

    Solved SQLite issue

--- app/src/main/java/ch/epfl/cs311/wanderwave/model/local/AppDatabase.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package ch.epfl.cs311.wanderwave.model.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [TrackEntity::class], version = 1)
+@Database(entities = [TrackEntity::class , BeaconEntity ::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
   abstract fun trackDao(): TrackDao
   abstract fun beaconDao(): BeaconDao
