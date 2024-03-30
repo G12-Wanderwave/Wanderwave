bcae6aa02c3eb65cb78681cd81b8f69acc45845e
bouaissim <menzo.bouaissi@epfl.ch>
2024-03-30 20:39:03 +0100

    ktfmtFormat

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/IconsActions.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/IconsActions.kt
@@ -1,6 +1,5 @@
 package ch.epfl.cs311.wanderwave.ui.components
 
-import android.util.Log
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -9,14 +8,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import ch.epfl.cs311.wanderwave.ui.navigation.NavigationActions
-import ch.epfl.cs311.wanderwave.ui.navigation.Route
-import ch.epfl.cs311.wanderwave.ui.navigation.TOP_LEVEL_DESTINATIONS
-import ch.epfl.cs311.wanderwave.viewmodel.ProfileViewModel
-
 
 /**
  * Handle the logic behind the edit button
@@ -33,16 +24,11 @@ fun ClickableIcon(
     modifier: Modifier,
     icon: ImageVector,
 ) {
-    IconButton(
-        modifier = modifier.then(Modifier.size(24.dp))
-            .testTag("clickableIcon"),
-        onClick = {
-            //TODO: Add the navController to navigate to the edit profile screen
-        }) {
-        Icon(
-            icon,
-            contentDescription = "Edit",
-            modifier = modifier
-        )
-    }
+  IconButton(
+      modifier = modifier.then(Modifier.size(24.dp)).testTag("clickableIcon"),
+      onClick = {
+        // TODO: Add the navController to navigate to the edit profile screen
+      }) {
+        Icon(icon, contentDescription = "Edit", modifier = modifier)
+      }
 }
