4dc6b6572673fa36c8a7674d8c8a10da27a0fd1d
tsogtb <circl3t@gmail.com>
2024-04-04 20:08:48 +0200

    adjust test

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MainPlaceHolder.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MainPlaceHolder.kt
@@ -1,9 +1,22 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
+import ch.epfl.cs311.wanderwave.navigation.Route
 
 @Composable
 fun MainPlaceHolder(navigationActions: NavigationActions) {
-  AboutScreen(navigationActions = navigationActions)
+  Column(modifier = Modifier.testTag("mainPlaceHolderScreen")) {
+    Button(
+        onClick = { navigationActions.navigateToTopLevel(Route.LOGIN) },
+        modifier = Modifier.testTag("signOutButton")) {
+          Text(text = "Sign Out")
+        }
+    AboutScreen(navigationActions = navigationActions)
+  }
 }
