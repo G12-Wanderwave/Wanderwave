5f7e126954e179767b61759ef504c6fe95f470d1
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-07 23:10:27 +0200

    bug fix

--- app/src/main/java/ch/epfl/cs311/wanderwave/model/localDb/AppDatabase.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/localDb/AppDatabase.kt
@@ -2,10 +2,12 @@ package ch.epfl.cs311.wanderwave.model.localDb
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ch.epfl.cs311.wanderwave.model.local.BeaconDao
+import ch.epfl.cs311.wanderwave.model.local.BeaconEntity
 
-@Database(entities = [TrackEntity::class, ProfileEntity::class], version = 1)
+@Database(entities = [TrackEntity::class, ProfileEntity::class, BeaconEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
   abstract fun trackDao(): TrackDao
-
+  abstract fun beaconDao(): BeaconDao
   abstract fun profileDao(): ProfileDao
 }
--- app/src/main/java/ch/epfl/cs311/wanderwave/model/localDb/LocalBeaconRepository.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/localDb/LocalBeaconRepository.kt
@@ -2,6 +2,7 @@ package ch.epfl.cs311.wanderwave.model.local
 
 import ch.epfl.cs311.wanderwave.model.data.Beacon
 import ch.epfl.cs311.wanderwave.model.data.Location
+import ch.epfl.cs311.wanderwave.model.localDb.AppDatabase
 import ch.epfl.cs311.wanderwave.model.repository.BeaconRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
