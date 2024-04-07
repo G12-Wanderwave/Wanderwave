0a488e48600dcd1d4b445b5bd88c773401a74f70
tsogtb <circl3t@gmail.com>
2024-04-08 00:52:23 +0200

    match main for real

--- app/src/main/java/ch/epfl/cs311/wanderwave/model/local/TrackDao.kt
+++
@@ -1,9 +0,0 @@
-package ch.epfl.cs311.wanderwave.model.local
-
-import androidx.room.Dao
-import androidx.room.Query
-
-@Dao
-interface TrackDao {
-  @Query("SELECT * FROM tracks") fun getAll(): List<TrackEntity>
-}
