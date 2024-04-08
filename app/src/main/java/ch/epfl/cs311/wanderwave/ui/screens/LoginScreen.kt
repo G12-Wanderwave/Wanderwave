c66e1f47a1003d390dd966c058561f99caf1f968
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-08 16:40:20 +0200

    managing to retreive a reference

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LoginScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/LoginScreen.kt
@@ -2,7 +2,9 @@ package ch.epfl.cs311.wanderwave.ui.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -16,7 +18,9 @@ import ch.epfl.cs311.wanderwave.ui.components.login.LoginAppLogo
 import ch.epfl.cs311.wanderwave.ui.components.login.SignInButton
 import ch.epfl.cs311.wanderwave.ui.components.login.WelcomeTitle
 import ch.epfl.cs311.wanderwave.viewmodel.ProfileViewModel
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
 
 @Composable
 fun LoginScreen(navigationActions: NavigationActions) {
@@ -48,12 +52,30 @@ fun LoginScreen(navigationActions: NavigationActions) {
 
       val beacon: Beacon = Beacon(id = "1234", location = Location(12.0, 12.0, "srilanka"), tracks = listOf())
 
-      db.collection("beacons")
-        .add(beacon)
-        .addOnFailureListener { e -> Log.e("Firestore", "Error adding document: ", e) }
-        .addOnSuccessListener { Log.d("Firestore", "DocumentSnapshot successfully added!") }
-    }) {
+      // db.collection("beacons")
+      //   .add(beacon)
+      //   .addOnFailureListener { e -> Log.e("Firestore", "Error adding document: ", e) }
+      //   .addOnSuccessListener { Log.d("Firestore", "DocumentSnapshot successfully added!") }
 
+      val dataFlow = MutableStateFlow(null)
+      db.collection("beacons")
+        .document("EmSELs5dY9UsPyyrNvIX")
+        .get()
+        .addOnSuccessListener { document ->
+          if (document != null && document.data != null) {
+            val trackRefs = document.get("tracks") as? List<DocumentReference>
+            trackRefs?.forEach { trackRef ->
+                trackRef.get().addOnSuccessListener { trackDocument ->
+                    val trackData = trackDocument.data
+                    Log.d("Firestore", "Track data: $trackData")
+                }
+            }
+          }
+        }
+        .addOnFailureListener { e -> Log.e("Firestore", "Error getting document: ", e) }
+
+    }, modifier = Modifier.fillMaxWidth()) {
+      Text(text = "Test Button")
     }
   }
 }
