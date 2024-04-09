0029a3889da12d44a2190a961be56ca5ee00789d
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-09 20:20:18 +0200

    format + solve app screen problem

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/AppTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/AppTest.kt
@@ -4,8 +4,14 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.cs311.wanderwave.MainActivity
+import ch.epfl.cs311.wanderwave.ui.screens.AppBottomBarScreen
 import ch.epfl.cs311.wanderwave.ui.screens.AppScreen
+import ch.epfl.cs311.wanderwave.ui.screens.LoginScreen
+import ch.epfl.cs311.wanderwave.ui.screens.MainPlaceHolder
+import ch.epfl.cs311.wanderwave.ui.screens.MapScreen
 import ch.epfl.cs311.wanderwave.ui.screens.SpotifyConnectScreen
+import ch.epfl.cs311.wanderwave.ui.screens.TrackListScreen
+import ch.epfl.cs311.wanderwave.ui.screens.components.MiniPlayerScreen
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -17,7 +23,8 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class AppTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
 
-  @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
+  @get:Rule
+  val composeTestRule = createAndroidComposeRule<MainActivity>()
 
   @Test
   fun appIsDisplayed() = run { onComposeScreen<AppScreen>(composeTestRule) { assertIsDisplayed() } }
@@ -51,6 +58,7 @@ class AppTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport
 
 
     fun appStartsAtConnectSpotifyScreen() = run {
-    onComposeScreen<SpotifyConnectScreen>(composeTestRule) { isDisplayed() }
+      onComposeScreen<SpotifyConnectScreen>(composeTestRule) { isDisplayed() }
+    }
   }
 }
