ad387749f8305710fc4d62829eb8d0ecd8dbc748
IB-12 <100189489+IB-12@users.noreply.github.com>
2024-04-09 18:06:22 +0200

    Redid my work on a fresh fork of main due to some weird issue on the previous branch

---
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/MapTest.kt
@@ -0,0 +1,39 @@
+package ch.epfl.cs311.wanderwave.ui
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.epfl.cs311.wanderwave.ui.screens.MapScreen
+import com.kaspersky.components.composesupport.config.withComposeSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class MapTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeSupport()) {
+
+  @get:Rule val composeTestRule = createAndroidComposeRule<TestActivity>()
+
+  @Before
+  fun setup() {
+    composeTestRule.setContent { MapScreen() }
+  }
+
+  @Test
+  fun mapIsDisplayed() = run {
+    onComposeScreen<MapScreen>(composeTestRule) { assertIsDisplayed() }
+  }
+
+  @Test
+  fun beaconsAreDisplayed() = run {
+    onComposeScreen<MapScreen>(composeTestRule) {
+      //Note: There is unfortunately no way to check whether markers are displayed on the map
+      //due to the way GoogleMaps handles them.
+    }
+  }
+}
--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
@@ -4,10 +4,41 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import ch.epfl.cs311.wanderwave.model.data.Beacon
+import ch.epfl.cs311.wanderwave.model.data.Location
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
 
 @Composable
 @Preview
 fun MapScreen() {
-  GoogleMap(modifier = Modifier.testTag("mapScreen"))
+  //TODO: Replace with actual data once implemented
+  val mockBeacons = listOf<Beacon>(
+    Beacon("INM" , Location(46.51857556996283, 6.5631609607190775)),
+    Beacon("BC"  , Location(46.51857417773428, 6.5619195033506434)),
+    Beacon("STCC", Location(46.52298529087412, 6.564644391110982 )),
+    Beacon("RLC" , Location(46.51846723837138, 6.568149323030634 )))
+
+  GoogleMap(modifier = Modifier.testTag("mapScreen")){
+    DisplayBeacons(beacons = mockBeacons)
+  }
+}
+
+
+/**
+ * This is a Composable function that displays the beacons on the map. It takes a list of beacons as
+ * input and adds a marker for each beacon on the map.
+ *
+ * @param beacons The list of beacons to be displayed on the map.
+ */
+@Composable
+fun DisplayBeacons(beacons: List<Beacon>) {
+  // Add a marker for each beacon
+  beacons.forEach() {
+    Marker(
+      state = MarkerState(position = it.location.toLatLng()),
+      title = it.id,
+    )
+  }
 }
