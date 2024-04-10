e5f77144ebd96be41e90494b417d7001358a9516
tsogtb <circl3t@gmail.com>
2024-04-10 18:03:39 +0200

    KTFormat and revert to for loop implementation

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/screens/AppBottomBarScreen.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/screens/AppBottomBarScreen.kt
@@ -1,13 +1,16 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import ch.epfl.cs311.wanderwave.navigation.Route
 import io.github.kakaocup.compose.node.element.ComposeScreen
 
 class AppBottomBarScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
     ComposeScreen<AppBottomBarScreen>(
         semanticsProvider = semanticsProvider, viewBuilderAction = { hasTestTag("appBottomBar") }) {
 
-  val bottomAppBarTrackListButton = onNode { hasTestTag("bottomAppBarTrackListButton") }
-  val bottomAppBarMainPlaceHolderButton = onNode { hasTestTag("bottomAppBarMainPlaceHolderButton") }
-  val mapScreenButton = onNode { hasTestTag("bottomAppBarMapButton") }
+  val bottomAppBarTrackListButton = onNode {
+    hasTestTag("bottomAppBarButton" + Route.TRACK_LIST.routeString)
+  }
+  val bottomAppBarMainButton = onNode { hasTestTag("bottomAppBarButton" + Route.MAIN.routeString) }
+  val mapScreenButton = onNode { hasTestTag("bottomAppBarButton" + Route.MAP.routeString) }
 }
--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/AppBottomBar.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/AppBottomBar.kt
@@ -1,21 +1,24 @@
 package ch.epfl.cs311.wanderwave.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
 import ch.epfl.cs311.wanderwave.navigation.TOP_LEVEL_DESTINATIONS
 
@@ -39,34 +42,19 @@ val mapIcon: ImageVector = Icons.Default.LocationOn
  */
 @Composable
 fun AppBottomBar(navActions: NavigationActions) {
-  BottomAppBar(
-      modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.surface).testTag("appBottomBar"),
-  ) {
-    // Assumes TOP_LEVEL_DESTINATIONS are in the order of Track List, Main, and Map for indexing
-    IconButton(
-        onClick = { navActions.navigateToTopLevel(TOP_LEVEL_DESTINATIONS[0].route) },
-        modifier =
-            Modifier.weight(1f) // Spread the icons evenly across the BottomAppBar
-                .testTag("bottomAppBarTrackListButton")) {
-          Icon(
-              trackListIcon,
-              contentDescription = stringResource(id = TOP_LEVEL_DESTINATIONS[0].textId))
-        }
-
-    Spacer(Modifier.weight(1f)) // Spacer for centering the middle icon
-
-    IconButton(
-        onClick = { navActions.navigateToTopLevel(TOP_LEVEL_DESTINATIONS[1].route) },
-        modifier = Modifier.weight(1f).testTag("bottomAppBarMainPlaceHolderButton")) {
-          Icon(mainIcon, contentDescription = stringResource(id = TOP_LEVEL_DESTINATIONS[1].textId))
-        }
-
-    Spacer(Modifier.weight(1f)) // Spacer for centering the middle icon
-
-    IconButton(
-        onClick = { navActions.navigateToTopLevel(TOP_LEVEL_DESTINATIONS[2].route) },
-        modifier = Modifier.weight(1f).testTag("bottomAppBarMapButton")) {
-          Icon(mapIcon, contentDescription = stringResource(id = TOP_LEVEL_DESTINATIONS[2].textId))
-        }
+  Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.testTag("appBottomBar")) {
+    BottomAppBar(
+        modifier = Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.surface),
+    ) {
+      for (destination in TOP_LEVEL_DESTINATIONS) {
+        Button(
+            onClick = { navActions.navigateToTopLevel(destination.route) },
+            modifier =
+                Modifier.padding(8.dp)
+                    .testTag("bottomAppBarButton" + destination.route.routeString)) {
+              Text(text = stringResource(id = destination.textId))
+            }
+      }
+    }
   }
 }
