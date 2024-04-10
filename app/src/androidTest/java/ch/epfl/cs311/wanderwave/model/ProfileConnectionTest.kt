2892f09db37b3665c9498710d2fce7965fd4deb5
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-10 22:05:59 +0200

    format

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/model/ProfileConnectionTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/model/ProfileConnectionTest.kt
@@ -3,17 +3,7 @@ package ch.epfl.cs311.wanderwave.model
 import android.util.Log
 import ch.epfl.cs311.wanderwave.model.data.Profile
 import ch.epfl.cs311.wanderwave.model.remote.ProfileConnection
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tasks.Tasks
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
-import io.mockk.MockKAnnotations
-import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
-import io.mockk.mockk
-import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
@@ -38,47 +28,32 @@ public class ProfileConnectionTest {
 
   @get:Rule val mockkRule = MockKRule(this)
   private lateinit var profileConnection: ProfileConnection
-  @RelaxedMockK private lateinit var firebaseFirestore: FirebaseFirestore
 
   @Before
   fun setup() {
-    MockKAnnotations.init(this)
-
-    profileConnection = ProfileConnection(firebaseFirestore)
+    profileConnection = ProfileConnection()
   }
 
-  // @Test
-  // fun testAddAndGetItem() = runBlocking {
-  //   withTimeout(30000) {
-  //     // Given
-  //     val profile = Profile(
-  //       firstName = "My FirstName",
-  //       lastName = "My LastName",
-  //       description = "My Description",
-  //       numberOfLikes = 0,
-  //       isPublic = true,
-  //       profilePictureUri = null,
-  //       firebaseUid = "My Firebase UID",
-  //       spotifyUid = "My Spotify UID"
-  //     )
-  //
-  //     // Mock the behavior of FirebaseFirestore
-  //     val documentReference = mockk<DocumentReference>()
-  //     val documentSnapshot = mockk<DocumentSnapshot>()
-  //     val task = mockk<Task<DocumentSnapshot>>()
-  //     every { task.addOnFailureListener(any()) } returns task
-  //     every { documentSnapshot.toObject(Profile::class.java) } returns profile
-  //     every { firebaseFirestore.collection(any()).document(any()) } returns documentReference
-  //     every { documentReference.set(profile) } returns mockk()
-  //     every { documentReference.get() } returns Tasks.forResult(documentSnapshot)
-  //     every { documentReference.set(any()) } returns mockk()
-  //
-  //     // When
-  //     profileConnection.addItemWithId(profile)
-  //     val retrievedProfile = profileConnection.getItem("testProfile").first()
-  //
-  //     // Then
-  //     assertEquals(profile, retrievedProfile)
-  //   }
-  // }
+  @Test
+  fun testAddAndGetItem() = runBlocking {
+    withTimeout(30000) {
+      var profile =
+          Profile(
+              firstName = "My FirstName",
+              lastName = "My LastName",
+              description = "My Description",
+              numberOfLikes = 0,
+              isPublic = true,
+              profilePictureUri = null,
+              firebaseUid = "My Firebase UID",
+              spotifyUid = "My Spotify UID")
+
+      profileConnection.addItemWithId(profile)
+
+      Log.d("Firestore", "Added item")
+      val retrievedProfile = profileConnection.getItem("testProfile").first()
+
+      assert(profile == retrievedProfile)
+    }
+  }
 }
