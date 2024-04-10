bc7f8a6ef427855441c33bb7724c02dad8c8ecab
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-10 12:35:53 +0200

    add custom LocationSource for more frequent updates

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/MapScreen.kt
@@ -4,7 +4,10 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
@@ -26,6 +29,7 @@ import com.google.accompanist.permissions.MultiplePermissionsState
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.CameraPositionState
@@ -74,12 +78,14 @@ fun MapScreen() {
   val cameraPositionState: CameraPositionState = rememberCameraPositionState() {}
   val mapIsLoaded = remember { mutableStateOf(false) }
 
+  val locationSource = CustomLocationSource(LocalContext.current)
   GoogleMap(
       modifier = Modifier.testTag("mapScreen"),
       properties =
           MapProperties(
               isMyLocationEnabled = permissionState.allPermissionsGranted,
           ),
+      locationSource = locationSource,
       cameraPositionState = cameraPositionState,
       onMapLoaded = { mapIsLoaded.value = true }) {}
 
@@ -97,8 +103,34 @@ fun MapScreen() {
     val location = getLastKnownLocation(LocalContext.current)
     LaunchedEffect(location != null, mapIsLoaded.value) {
       cameraPositionState.move(
-          CameraUpdateFactory.newCameraPosition(
-              CameraPosition.fromLatLngZoom(location!!, 15f)))
+          CameraUpdateFactory.newCameraPosition(CameraPosition.fromLatLngZoom(location!!, 15f)))
     }
+  }
+}
+
+class CustomLocationSource(private val context: Context) : LocationSource, LocationListener {
+  private var listener: LocationSource.OnLocationChangedListener? = null
+  private var locationManager: LocationManager? = null
+
+  @RequiresApi(Build.VERSION_CODES.S)
+  @RequiresPermission(
+      allOf =
+          [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
+  override fun activate(onLocationChangedListener: LocationSource.OnLocationChangedListener) {
+    listener = onLocationChangedListener
+    locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    println("Location source activated. Providers: ${locationManager?.allProviders}")
+    locationManager?.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 0, 0f, this)
+  }
+
+  override fun deactivate() {
+    locationManager?.removeUpdates(this)
+    locationManager = null
+    listener = null
+  }
+
+  override fun onLocationChanged(location: Location) {
+    println("Location changed: $location")
+    listener?.onLocationChanged(location)
   }
 }
