b70b4e501c7349229d5d81aee0b48f2ee8f924d2
IB-12 <100189489+IB-12@users.noreply.github.com>
2024-04-10 23:08:50 +0200

    Added tests

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/EditProfileTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/EditProfileTest.kt
@@ -2,16 +2,17 @@ package ch.epfl.cs311.wanderwave.ui
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import ch.epfl.cs311.wanderwave.model.data.Profile
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
+import ch.epfl.cs311.wanderwave.navigation.Route
 import ch.epfl.cs311.wanderwave.ui.screens.EditProfileScreen
+import ch.epfl.cs311.wanderwave.ui.screens.ProfileScreen
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import dagger.hilt.android.testing.HiltAndroidTest
-import io.github.kakaocup.compose.node.element.ComposeScreen
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
 import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -63,6 +64,18 @@ class EditProfileTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
       saveButton {
         assertIsDisplayed()
         performClick()
+        onComposeScreen<ProfileScreen>(composeTestRule) { assertIsDisplayed() }
+      }
+    }
+  }
+
+  @Test
+  fun cancelEdit() = run {
+    onComposeScreen<EditProfileScreen>(composeTestRule) {
+      cancelButton {
+        assertIsDisplayed()
+        performClick()
+        verify { mockNavigationActions.navigateToTopLevel(Route.PROFILE) }
       }
     }
   }
@@ -73,7 +86,10 @@ class EditProfileTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withCompos
       deleteButton {
         assertIsDisplayed()
         performClick()
+        verify { mockNavigationActions.navigateToTopLevel(Route.LOGIN) }
       }
     }
   }
+
+
 }
