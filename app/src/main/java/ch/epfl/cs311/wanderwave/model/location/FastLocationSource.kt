a557ac992f9cc321324aac73fe4fa10919cedac8
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-11 17:17:36 +0200

    explicitly use the HIGH PRIORITY location request

--- app/src/main/java/ch/epfl/cs311/wanderwave/model/location/FastLocationSource.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/location/FastLocationSource.kt
@@ -3,16 +3,19 @@ package ch.epfl.cs311.wanderwave.model.location
 import android.Manifest
 import android.content.Context
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.annotation.RequiresPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.LocationSource
 
 class FastLocationSource(private val context: Context) : LocationSource, LocationListener {
   private var listener: LocationSource.OnLocationChangedListener? = null
-  private var locationManager: LocationManager? = null
+  private var locationClient: FusedLocationProviderClient? = null
 
   @RequiresApi(Build.VERSION_CODES.S)
   @RequiresPermission(
@@ -20,13 +23,16 @@ class FastLocationSource(private val context: Context) : LocationSource, Locatio
           [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION])
   override fun activate(onLocationChangedListener: LocationSource.OnLocationChangedListener) {
     listener = onLocationChangedListener
-    locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-    locationManager?.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 0, 0f, this)
+    locationClient = LocationServices.getFusedLocationProviderClient(context)
+    val locationRequest = LocationRequest.Builder(5)
+      .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+      .build()
+    locationClient?.requestLocationUpdates(locationRequest, this, null)
   }
 
   override fun deactivate() {
-    locationManager?.removeUpdates(this)
-    locationManager = null
+    locationClient?.removeLocationUpdates(this)
+    locationClient = null
     listener = null
   }
 
