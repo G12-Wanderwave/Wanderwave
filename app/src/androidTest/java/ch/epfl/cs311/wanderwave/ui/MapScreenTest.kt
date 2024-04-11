b6953d9acf0b818535a07381c3e06ff1fa98f970
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-11 14:41:51 +0200

    add another test

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/MapScreenTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/ui/MapScreenTest.kt
@@ -3,20 +3,25 @@ package ch.epfl.cs311.wanderwave.ui
 import android.Manifest
 import android.content.Context
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
+import ch.epfl.cs311.wanderwave.model.location.FastLocationSource
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
 import ch.epfl.cs311.wanderwave.ui.screens.MapScreen
 import ch.epfl.cs311.wanderwave.viewmodel.MapViewModel
+import com.google.android.gms.maps.LocationSource
 import com.kaspersky.components.composesupport.config.withComposeSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import io.github.kakaocup.compose.node.element.ComposeScreen.Companion.onComposeScreen
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
+import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Before
 import org.junit.Rule
@@ -41,22 +46,23 @@ class MapScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeS
           Manifest.permission.ACCESS_FINE_LOCATION,
       )
 
+  private val context = InstrumentationRegistry.getInstrumentation().context
+
   private val locationManager =
-      InstrumentationRegistry.getInstrumentation()
-          .context
-          .getSystemService(Context.LOCATION_SERVICE) as LocationManager
+      context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+  val location =
+      Location(LocationManager.GPS_PROVIDER).apply {
+        latitude = 46.519962
+        longitude = 6.633597
+        time = System.currentTimeMillis()
+        elapsedRealtimeNanos = System.nanoTime()
+      }
 
   @Before
   fun setup() {
     composeTestRule.setContent { MapScreen(mockNavigationActions, mockMapViewModel) }
 
-    val location =
-        Location(LocationManager.GPS_PROVIDER).apply {
-          latitude = 46.519962
-          longitude = 6.633597
-          time = System.currentTimeMillis()
-          elapsedRealtimeNanos = System.nanoTime()
-        }
     try {
       locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true)
       locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, location)
@@ -74,5 +80,28 @@ class MapScreenTest : TestCase(kaspressoBuilder = Kaspresso.Builder.withComposeS
   fun locationCallbackIsCalled() = run {
     val viewModel = mockMapViewModel
     verify { viewModel.locationSource }
+  }
+
+  @Test
+  fun locationSourceCallsActivate() = run {
+    val viewModel = mockMapViewModel
+    verify { viewModel.locationSource.activate(any()) }
+  }
+
+  @Test
+  fun locationSourceCallsOnLocationChanged() = run {
+    val mockListener = mockk<LocationSource.OnLocationChangedListener>()
+    every { mockListener.onLocationChanged(any()) } returns Unit
+    val mockContext = mockk<Context>()
+    val locationManager = mockk<LocationManager>()
+    every { mockContext.getSystemService(Context.LOCATION_SERVICE) } returns locationManager
+    every {
+      locationManager.requestLocationUpdates(
+          any<String>(), any<Long>(), any(), any<LocationListener>())
+    } returns Unit
+    val source = FastLocationSource(mockContext)
+    source.activate(mockListener)
+    source.onLocationChanged(location)
+    verify { mockListener.onLocationChanged(any()) }
   }
 }
