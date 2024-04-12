11afe0030c914a6e7a88839b4003b462d35b5889
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-12 12:48:12 +0200

    extract a separate BeaconScreen Function to allow for a preview

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
@@ -1,11 +1,24 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
 import ch.epfl.cs311.wanderwave.viewmodel.BeaconViewModel
 
 @Composable
 fun BeaconScreen(navigationActions: NavigationActions, viewModel: BeaconViewModel = hiltViewModel()) {
+  BeaconScreen()
+}
 
+@Composable
+@Preview
+private fun BeaconScreen() {
+  Column(modifier = Modifier.fillMaxSize()) {
+    Text(text = "Beacon")
+  }
 }
