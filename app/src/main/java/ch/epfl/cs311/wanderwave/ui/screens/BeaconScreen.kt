007d0a7007085b01e1fdc259ad1407d787f45ea2
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-12 10:54:54 +0200

    Beacon Screen skeleton

---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
@@ -0,0 +1,11 @@
+package ch.epfl.cs311.wanderwave.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import ch.epfl.cs311.wanderwave.navigation.NavigationActions
+import ch.epfl.cs311.wanderwave.viewmodel.BeaconViewModel
+
+@Composable
+fun BeaconScreen(navigationActions: NavigationActions, viewModel: BeaconViewModel = hiltViewModel()) {
+
+}
---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
@@ -0,0 +1,8 @@
+package ch.epfl.cs311.wanderwave.viewmodel
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class BeaconViewModel @Inject constructor() : ViewModel() {
+
+}
