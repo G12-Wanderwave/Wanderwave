91f7746651fe57eacca5c98551cb654d255c052f
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-13 01:50:22 +0200

    ui can retreive a beacon with the id

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
@@ -8,11 +8,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import ch.epfl.cs311.wanderwave.model.data.Beacon
+import ch.epfl.cs311.wanderwave.model.data.Track
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
 import ch.epfl.cs311.wanderwave.ui.theme.WanderwaveTheme
 import ch.epfl.cs311.wanderwave.viewmodel.BeaconViewModel
@@ -22,7 +30,26 @@ fun BeaconScreen(
     navigationActions: NavigationActions,
     viewModel: BeaconViewModel = hiltViewModel()
 ) {
-  BeaconScreen()
+  // id value remebered for the text field
+  // Here is the id of a good beacon for testing : UAn8OUadgrUOKYagf8a2
+  var id by remember { mutableStateOf("") }
+  var beacon = viewModel.beacon.collectAsState(initial = null)
+  Column(
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(16.dp),
+    horizontalAlignment = Alignment.CenterHorizontally) {
+    TextField(value = id, onValueChange = { id = it})
+    Button(onClick = {
+      viewModel.getBeaconById(id)
+    }) { Text("Get Beacon") }
+
+    BeaconInformation(beacon)
+    SongList(beacon)
+
+
+
+  }
 }
 
 @Composable
@@ -33,35 +60,30 @@ private fun BeaconScreenPreview() {
 
 @Composable
 private fun BeaconScreen() {
-  Column(
-      modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp),
-      horizontalAlignment = Alignment.CenterHorizontally) {
-        BeaconInformation()
-        SongList()
-
-      }
 }
 
 @Composable
-fun BeaconGetter() {
-  // Form with a text input and a button
-  Column() {
-    TextField(value = "", onValueChange = { /*TODO*/ })
-    Button(onClick = { /*TODO*/ }) { Text("Get Beacon") }
-  }
-}
-
-@Composable
-fun BeaconInformation() {
+fun BeaconInformation(beacon: State<Beacon?>) {
   Column(horizontalAlignment = Alignment.CenterHorizontally) {
     Text("Beacon", style = MaterialTheme.typography.displayMedium)
-    Text("Beacon Information")
+    beacon.value?.let {
+      Text("Beacon: ${it.id}")
+    }
+    // location
+    beacon.value?.let {
+      val location = it.location
+      Text("Location: lat:${location.latitude}, long:${location.longitude}")
+    }
   }
 }
 
 @Composable
-fun SongList() {
-  Column() { Text("Song List") }
+fun SongList(beacon: State<Beacon?>) {
+  // songs
+  beacon.value?.let {
+    val tracks:List<Track> = it.tracks
+    tracks.map { track ->
+      Text("Track: ${track.title}")
+    }
+  }
 }
--- app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
@@ -1,6 +1,29 @@
 package ch.epfl.cs311.wanderwave.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ch.epfl.cs311.wanderwave.model.data.Beacon
+import ch.epfl.cs311.wanderwave.model.remote.BeaconConnection
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class BeaconViewModel @Inject constructor() : ViewModel() {}
+class BeaconViewModel @Inject constructor() : ViewModel() {
+  val beaconConnection = BeaconConnection()
+
+  private val _beacon = MutableStateFlow<Beacon?>(null)
+  val beacon: StateFlow<Beacon?> = _beacon
+
+  fun getBeaconById(id: String) {
+    viewModelScope.launch {
+      beaconConnection.getItem(id).collect { fetchedBeacon ->
+        _beacon.value = fetchedBeacon
+      }
+    }
+  }
+
+}
