330c6c023898b1faa9ff7495f12d5d35669101be
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-12 16:05:07 +0200

    add a separate @Preview composable

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
@@ -2,12 +2,15 @@ package ch.epfl.cs311.wanderwave.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
+import ch.epfl.cs311.wanderwave.ui.theme.WanderwaveTheme
 import ch.epfl.cs311.wanderwave.viewmodel.BeaconViewModel
 
 @Composable
@@ -19,7 +22,21 @@ fun BeaconScreen(
 }
 
 @Composable
-@Preview
-private fun BeaconScreen() {
-  Column(modifier = Modifier.fillMaxSize()) { Text(text = "Beacon") }
+@Preview(showBackground = true)
+private fun BeaconScreenPreview() {
+  WanderwaveTheme { BeaconScreen() }
 }
+
+@Composable
+private fun BeaconScreen() {
+  Column(
+      modifier = Modifier.fillMaxSize().padding(16.dp),
+      horizontalAlignment = Alignment.CenterHorizontally) {
+        BeaconInformation()
+        SongList()
+      }
+}
+
+@Composable fun BeaconInformation() {}
+
+@Composable fun SongList() {}
