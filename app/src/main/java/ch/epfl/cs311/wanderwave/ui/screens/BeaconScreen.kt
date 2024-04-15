4315629d7d26f64020ee5bbcf04fc93e5b40ef6f
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2024-04-15 15:32:08 +0200

    add track list and map to beacon screen
    Since there is no proper navigation yet (Issue #133), the way to test the UI at the moment is
    by either faking the navigation (aka setting the beacon screen as the start screen) or
    via the preview.

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/screens/BeaconScreen.kt
@@ -1,15 +1,26 @@
 package ch.epfl.cs311.wanderwave.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -27,6 +39,7 @@ import ch.epfl.cs311.wanderwave.model.data.Track
 import ch.epfl.cs311.wanderwave.navigation.NavigationActions
 import ch.epfl.cs311.wanderwave.ui.theme.WanderwaveTheme
 import ch.epfl.cs311.wanderwave.viewmodel.BeaconViewModel
+import com.google.maps.android.compose.GoogleMap
 
 @Composable
 fun BeaconScreen(
@@ -35,21 +48,15 @@ fun BeaconScreen(
 ) {
   // id value remebered for the text field
   // Here is the id of a good beacon for testing : UAn8OUadgrUOKYagf8a2
-  var id by remember { mutableStateOf("UAn8OUadgrUOKYagf8a2") }
-  val beacon = viewModel.uiState.collectAsState()
+  val uiState = viewModel.uiState.collectAsState().value
   Column(
     modifier = Modifier
       .fillMaxSize()
       .padding(16.dp),
     horizontalAlignment = Alignment.CenterHorizontally
   ) {
-    TextField(value = id, onValueChange = { id = it }, placeholder = { Text("Beacon ID") })
-    Button(onClick = {
-      viewModel.getBeaconById(id)
-    }) { Text("Get Beacon") }
-
-    if (!beacon.value.isLoading) {
-      BeaconScreen(beacon = beacon.value.beacon!!)
+    if (!uiState.isLoading) {
+      BeaconScreen(beacon = uiState.beacon!!)
     } else {
       Text("Beacon not found")
     }
@@ -88,25 +95,66 @@ private fun BeaconScreen(beacon: Beacon) {
 @Composable
 fun BeaconInformation(location: Location) {
   Column(horizontalAlignment = Alignment.CenterHorizontally) {
-    Text("Beacon", style = MaterialTheme.typography.displayMedium)
+    Text("Beacon", style = MaterialTheme.typography.displayLarge)
     val locationText = if (location.name != "") {
       "${location.name} (${location.latitude}, ${location.longitude})"
     } else {
       "(${location.latitude}, ${location.longitude})"
     }
     Text("at $locationText", style = MaterialTheme.typography.titleMedium)
+    // TODO: Add a proper GoogleMap here (with the proper UI and attributes).
+    GoogleMap(
+      modifier = Modifier
+        .fillMaxWidth()
+        .aspectRatio(4f / 3)
+        .padding(4.dp)
+        .clip(RoundedCornerShape(8.dp))
+    )
   }
 }
 
 @Composable
 fun SongList(beacon: Beacon) {
+  HorizontalDivider()
+  Text(text = "Tracks", style = MaterialTheme.typography.displayMedium)
   LazyColumn {
-    items(beacon.tracks) { track ->
-      Card(
-      ) {
+    items(beacon.tracks) { TrackItem(it) }
+  }
+}
+
+@Composable
+internal fun TrackItem(track: Track) {
+  Card(
+    colors = CardColors(
+      containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+      CardDefaults.cardColors().contentColor,
+      CardDefaults.cardColors().disabledContainerColor,
+      CardDefaults.cardColors().disabledContentColor
+    ),
+    modifier = Modifier
+      .height(80.dp)
+      .fillMaxWidth()
+      .padding(4.dp)) {
+    Row(
+      verticalAlignment = Alignment.CenterVertically,
+    ) {
+      Box(modifier = Modifier
+        .fillMaxHeight()
+        .aspectRatio(1f), contentAlignment = Alignment.Center) {
+        Image(imageVector = Icons.Default.PlayArrow, contentDescription = "Album Cover",
+          modifier = Modifier.fillMaxSize(.8f),
+        )
+      }
+      Column(modifier = Modifier.padding(8.dp)) {
         Text(
-          text = "Track: ${track.title} by ${track.artist}",
-          modifier = Modifier.padding(8.dp),
+          text = track.title,
+          color = MaterialTheme.colorScheme.onSurface,
+          style = MaterialTheme.typography.titleMedium
+        )
+        Text(
+          text = track.artist,
+          color = MaterialTheme.colorScheme.onSurfaceVariant,
+          style = MaterialTheme.typography.bodyMedium,
         )
       }
     }
--- app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.epfl.cs311.wanderwave.model.data.Beacon
 import ch.epfl.cs311.wanderwave.model.remote.BeaconConnection
+import dagger.Provides
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,11 +14,17 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class BeaconViewModel @Inject constructor() : ViewModel() {
   private val beaconConnection = BeaconConnection()
+  private val id = "UAn8OUadgrUOKYagf8a2"
 
-  private var _uiState = MutableStateFlow<UIState>(UIState())
+  private var _uiState = MutableStateFlow(UIState())
   val uiState: StateFlow<UIState> = _uiState
+
+  init {
+    getBeaconById(id)
+  }
 
   fun getBeaconById(id: String) {
     viewModelScope.launch {
