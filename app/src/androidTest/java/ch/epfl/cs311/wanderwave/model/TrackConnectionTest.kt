0daa383f8372ad9341a7f6c8b85032586243527d
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-22 19:47:30 +0200

    implement get with catch for track

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/model/TrackConnectionTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/model/TrackConnectionTest.kt
@@ -2,14 +2,21 @@ package ch.epfl.cs311.wanderwave.model
 
 import ch.epfl.cs311.wanderwave.model.data.Track
 import ch.epfl.cs311.wanderwave.model.remote.TrackConnection
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.junit4.MockKRule
 import io.mockk.mockk
 import io.mockk.verify
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -62,13 +69,43 @@ class TrackConnectionTest {
   }
 
   @Test
-  fun testGetItem() = runBlocking {
-    // Call the function under test
-    val retrievedTrack = trackConnection.getItem("testTrack")
+fun testGetItem() = runBlocking {
+    withTimeout(3000) {
+        // Mock the Task
+        val mockTask = mockk<Task<DocumentSnapshot>>()
+        val mockDocumentSnapshot = mockk<DocumentSnapshot>()
 
-    // Verify that the get function is called on the document with the correct id
-    verify { documentReference.get() }
-  }
+        val getTestTrack = Track("testTrack", "Test Title", "Test Artist")
+
+        every { mockDocumentSnapshot.getData() } returns getTestTrack.toMap()
+        every { mockDocumentSnapshot.exists() } returns true
+        every { mockDocumentSnapshot.id } returns getTestTrack.id
+        every { mockDocumentSnapshot.getString("title") } returns getTestTrack.title
+        every { mockDocumentSnapshot.getString("artist") } returns getTestTrack.artist
+
+        // Define behavior for the addOnSuccessListener method
+        every { mockTask.addOnSuccessListener(any<OnSuccessListener<DocumentSnapshot>>()) } answers {
+            val listener = arg<OnSuccessListener<DocumentSnapshot>>(0)
+
+            // Define the behavior of the mock DocumentSnapshot here
+            listener.onSuccess(mockDocumentSnapshot)
+            mockTask
+        }
+        every { mockTask.addOnFailureListener(any()) } answers {
+            mockTask
+        }
+
+        // Define behavior for the get() method on the DocumentReference to return the mock task
+        every { documentReference.get() } returns mockTask
+
+        // Call the function under test
+        val retrievedTrack = trackConnection.getItem("testTrack").first()
+
+        // Verify that the get function is called on the document with the correct id
+        coVerify { documentReference.get() }
+        assertEquals(getTestTrack, retrievedTrack)
+    }
+}
 
   @Test
   fun testDeleteItem() {
