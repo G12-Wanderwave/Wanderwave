5c12e783afa114bd881974d724a1d53355351da8
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-22 20:06:27 +0200

    implement changes

---
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/localDb/AppDatabase.kt
@@ -0,0 +1,21 @@
+package ch.epfl.cs311.wanderwave.model.localDb
+
+import androidx.room.ColumnInfo
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.RoomDatabase
+
+
+
+@Database(entities = [PlaceHolderEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+}
+
+// place holder entity
+@Entity(tableName = "beacons")
+data class PlaceHolderEntity(
+  @PrimaryKey @ColumnInfo(name = "id") val id: String,
+  @ColumnInfo(name = "latitude") val latitude: Double,
+  @ColumnInfo(name = "longitude") val longitude: Double,
+)
