78fcaf50f387722faa92daaa4748c98e0b7eeb8c
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-23 22:15:07 +0200

    delete hard coded id

--- app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconViewModel.kt
@@ -3,6 +3,7 @@ package ch.epfl.cs311.wanderwave.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.epfl.cs311.wanderwave.model.data.Beacon
+import ch.epfl.cs311.wanderwave.model.data.Location
 import ch.epfl.cs311.wanderwave.model.repository.BeaconRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -13,14 +14,19 @@ import kotlinx.coroutines.launch
 @HiltViewModel
 class BeaconViewModel @Inject constructor(private val beaconRepository: BeaconRepository) :
     ViewModel() {
-  private val id = "UAn8OUadgrUOKYagf8a2"
 
   private var _uiState = MutableStateFlow(UIState())
 
   val uiState: StateFlow<UIState> = _uiState
 
   init {
-    getBeaconById(id)
+    val sampleBeacon = Beacon(
+        id = "Sample ID",
+        location = Location(0.0, 0.0),
+        tracks = emptyList()
+    )
+
+    _uiState.value = UIState(beacon = sampleBeacon, isLoading = false)
   }
 
   fun getBeaconById(id: String) {
