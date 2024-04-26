dd56374c3d3ac0260b211abcc1922e491feb0c44
Clarence LINDEN <clarence.linden@epfl.ch>
2024-04-26 17:40:28 +0200

    new beacon logo + put it on the map

--- app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/map/BeaconMapMarker.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/ui/components/map/BeaconMapMarker.kt
@@ -1,6 +1,10 @@
 package ch.epfl.cs311.wanderwave.ui.components.map
 
+import android.graphics.BitmapFactory
 import androidx.compose.runtime.Composable
+import ch.epfl.cs311.wanderwave.R
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMapComposable
 import com.google.maps.android.compose.Marker
@@ -15,11 +19,13 @@ fun BeaconMapMarker(
     onClick: () -> Unit = {}
 ) {
   Marker(
-      state = MarkerState(position = position),
-      title = title,
-      snippet = snippet,
-      onClick = {
-        onClick()
-        true
-      })
+    state = MarkerState(position = position),
+    title = title,
+    snippet = snippet,
+    onClick = {
+      onClick()
+      true
+    },
+    icon = BitmapDescriptorFactory.fromResource(R.drawable.beacon_logo)
+  )
 }
