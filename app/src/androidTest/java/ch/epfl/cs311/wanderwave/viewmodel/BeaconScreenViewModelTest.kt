407871dc87f8dc94cc4c103cefe76e82a2213bdb
IB-12 <100189489+IB-12@users.noreply.github.com>
2024-05-16 19:40:25 +0200

    Fixed tests

--- app/src/androidTest/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconScreenViewModelTest.kt
+++ app/src/androidTest/java/ch/epfl/cs311/wanderwave/viewmodel/BeaconScreenViewModelTest.kt
@@ -2,9 +2,12 @@ package ch.epfl.cs311.wanderwave.viewmodel
 
 import ch.epfl.cs311.wanderwave.model.data.Track
 import ch.epfl.cs311.wanderwave.model.remote.BeaconConnection
+import ch.epfl.cs311.wanderwave.model.spotify.SpotifyController
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
 import io.mockk.verify
+import kotlinx.coroutines.flow.flowOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,15 +18,18 @@ class BeaconScreenViewModelTest {
 
   @get:Rule val mockkRule = MockKRule(this)
   @RelaxedMockK private lateinit var beaconConnection: BeaconConnection
+  @RelaxedMockK private lateinit var mockSpotifyController: SpotifyController
 
   @Test
   fun canConstructWithNoErrors() {
-    BeaconViewModel(beaconConnection)
+    val connectResult = SpotifyController.ConnectResult.SUCCESS
+    every { mockSpotifyController.connectRemote() } returns flowOf(connectResult)
+    BeaconViewModel(beaconConnection, mockSpotifyController)
   }
 
   @Test
   fun addTrackToBeaconTest() {
-    val viewModel = BeaconViewModel(beaconConnection)
+    val viewModel = BeaconViewModel(beaconConnection, mockSpotifyController)
     val track = Track("trackId", "trackName", "trackArtist")
     viewModel.addTrackToBeacon("beaconId", track, {})
 
