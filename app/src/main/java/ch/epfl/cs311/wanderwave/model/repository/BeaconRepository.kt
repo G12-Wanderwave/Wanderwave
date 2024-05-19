2af5f161ffa614b833d92ae532e2bc172cf4a69b
Yoric Züger <79338025+yzueger@users.noreply.github.com>
2024-05-19 18:16:03 +0200

    Set the users profile correctly when adding a track to a beacon

--- app/src/main/java/ch/epfl/cs311/wanderwave/model/repository/BeaconRepository.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/model/repository/BeaconRepository.kt
@@ -9,7 +9,12 @@ interface BeaconRepository : FirebaseRepository<Beacon> {
 
   fun getAll(): Flow<List<Beacon>>
 
-  fun addTrackToBeacon(beaconId: String, track: Track, onComplete: (Boolean) -> Unit)
+  fun addTrackToBeacon(
+      beaconId: String,
+      track: Track,
+      profileUid: String,
+      onComplete: (Boolean) -> Unit
+  )
 
   suspend fun addItemAndGetId(item: Beacon): String?
 }
