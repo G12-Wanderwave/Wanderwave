8ff21f01802af260a53755a8d0db3a139e6f16be
Ayman <ayman.bakiri@epfl,ch>
2024-05-31 14:03:53 +0200

    ktfmt2

--- app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/SpotifyConnectScreenViewModel.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/SpotifyConnectScreenViewModel.kt
@@ -1,5 +1,6 @@
 package ch.epfl.cs311.wanderwave.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import ch.epfl.cs311.wanderwave.model.auth.AuthenticationController
 import ch.epfl.cs311.wanderwave.model.repository.ProfileRepository
@@ -43,8 +44,21 @@ constructor(
   }
 
   suspend fun checkIfFirstTime() {
-    val userId = authenticationController.getUserData()?.id ?: return
+    val userId = authenticationController.getUserData()?.id
+    if (userId == null) {
+      Log.i("SpotifyConnectVM", "User ID is null, exiting checkIfFirstTime")
+      return
+    }
+    Log.i("SpotifyConnectVM", "Fetching profile for user ID: $userId")
+
     val profileResult = profileRepository.getItem(userId).firstOrNull()
+    if (profileResult == null) {
+      Log.i("SpotifyConnectVM", "Profile result is null")
+    } else if (profileResult.isFailure) {
+      Log.e(
+          "SpotifyConnectVM", "Error fetching profile: ${profileResult.exceptionOrNull()?.message}")
+    }
+
     _isFirstTime.value =
         profileResult?.isFailure == true &&
             profileResult.exceptionOrNull()?.message == "Document does not exist"
