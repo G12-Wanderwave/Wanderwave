892f3ae28deb333d8d4b18ef6d760a85197e2bcb
Ayman <ayman.bakiri@epfl,ch>
2024-05-31 20:18:21 +0200

    ktfmt7

--- app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/SpotifyConnectScreenViewModel.kt
+++ app/src/main/java/ch/epfl/cs311/wanderwave/viewmodel/SpotifyConnectScreenViewModel.kt
@@ -6,11 +6,11 @@ import ch.epfl.cs311.wanderwave.model.auth.AuthenticationController
 import ch.epfl.cs311.wanderwave.model.repository.ProfileRepository
 import ch.epfl.cs311.wanderwave.model.spotify.SpotifyController
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.single
+import javax.inject.Inject
 
 @HiltViewModel
 class SpotifyConnectScreenViewModel
@@ -51,9 +51,9 @@ constructor(
     }
     Log.i("UserId", "UserID: $userId")
     val profileResult = profileRepository.getItem(userId).firstOrNull()
-    if (profileResult == null) {
-      Log.i("CheckFirstTime", "No profile result obtained")
-    }
+    // if (profileResult == null) {
+    //   Log.i("CheckFirstTime", "No profile result obtained")
+    // }
     _isFirstTime.value =
         profileResult?.isFailure == true &&
             profileResult.exceptionOrNull()?.message == "Document does not exist"
